e82144c11530c0dca5fe5b978f6442d6e862d607
alebaviera <baviera.alessandra@gmailcom>
2019-07-22 17:31:36 +0200

    clear msg input

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 $('.sendmsg').click(function(){
 
-  var messaggio = $('input').val();
+  var messaggio = $('.msg input').val();
 
 
   var newmsg = $('.chat').append('<div class="send">'+ messaggio + '</div>');
@@ -11,6 +11,8 @@ $('.sendmsg').click(function(){
   setTimeout(function (){
     var answ = $('.chat').append('<div class="received"> NON ORA!</div>');
   }, 1000);
+
+  messaggio= $('.msg input').val('');
 });
 
 
