283788697ce113cbd550cd60c71f63f3c768aa79
alebaviera <baviera.alessandra@gmailcom>
2019-07-23 15:16:22 +0200

    keypress sendmessage e searchcontact

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
 
 
-$('.sendmsg').click(function(){
+$('.sendmsg').click(sendmsg);
+
+
+var tasto = $('.msg input').keypress(function(){
+if(event.which == 13){
+  sendmsg();
+}
+});
+
+function sendmsg(){
 
   var messaggio = $('.msg input').val();
   var msgelementsend = $('.template .send').clone();
@@ -10,14 +19,46 @@ $('.sendmsg').click(function(){
   $('.chat').append(newmsg);
 
   setTimeout(function (){
-    var answ = msgelementreceived.text('NON ORA!');
+    var answ = msgelementreceived.text('non ora');
     var newansw = $('.chat').append(answ);
 
   }, 1000);
 
   messaggio= $('.msg input').val('');
+};
+
+
+$('.search input').keypress(function (){
+
+
+
+
+
+
+  $('.contatto').each(search);
+
+// funzione cerca contatto
+
+  function search(){
+
+    var letters = $('.search input').val().toUpperCase();
+
+    var contatto = $(this).children('.nome').text().toUpperCase();
+
+    if (contatto.includes(letters)){
+      console.log('qui sono nell-if', contatto);
+      $(this).show();
+    }
+    else{
+      $(this).hide();
+
+    }
+
+
+
+
+
+  }
 });
-
-
 
 });
